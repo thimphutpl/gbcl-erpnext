2de635fe62e03e0d59ad3c860f4d47c6686636dd
Sonam-555 <sonamdoji384@gmail.com>
2026-01-14 14:30:11 +0600

    Update employee group

--- erpnext/setup/doctype/employee_group/employee_group.py
+++ erpnext/setup/doctype/employee_group/employee_group.py
@@ -1,26 +1,48 @@
-# Copyright (c) 2018, Frappe Technologies Pvt. Ltd. and contributors
+# # Copyright (c) 2018, Frappe Technologies Pvt. Ltd. and contributors
+# # For license information, please see license.txt
+
+
+# from frappe.model.document import Document
+
+
+# class EmployeeGroup(Document):
+# 	# begin: auto-generated types
+# 	# This code is auto-generated. Do not modify anything in this block.
+
+# 	from typing import TYPE_CHECKING
+
+# 	if TYPE_CHECKING:
+# 		from erpnext.setup.doctype.employee_group_table.employee_group_table import EmployeeGroupTable
+# 		from frappe.types import DF
+
+# 		employee_group_name: DF.Data
+# 		employee_list: DF.Table[EmployeeGroupTable]
+# 		employee_pf: DF.Percent
+# 		employer_pf: DF.Percent
+# 		health_contribution: DF.Currency
+# 		minimum_months: DF.Float
+# 	# end: auto-generated types
+
+# 	pass
+
+
+# -*- coding: utf-8 -*-
+# Copyright (c) 2015, Frappe Technologies Pvt. Ltd. and contributors
 # For license information, please see license.txt
 
-
+from __future__ import unicode_literals
+import frappe
+from frappe import _
 from frappe.model.document import Document
 
-
 class EmployeeGroup(Document):
-	# begin: auto-generated types
-	# This code is auto-generated. Do not modify anything in this block.
+	def validate(self):
+		self.validate_group_items()
 
-	from typing import TYPE_CHECKING
-
-	if TYPE_CHECKING:
-		from erpnext.setup.doctype.employee_group_table.employee_group_table import EmployeeGroupTable
-		from frappe.types import DF
-
-		employee_group_name: DF.Data
-		employee_list: DF.Table[EmployeeGroupTable]
-		employee_pf: DF.Percent
-		employer_pf: DF.Percent
-		health_contribution: DF.Currency
-		minimum_months: DF.Float
-	# end: auto-generated types
-
-	pass
+	def validate_group_items(self):
+		dup = {}
+		for i in self.items:
+			if i.leave_type in dup:
+				frappe.throw(_("Row#{0} : Duplicate Record found for leave type `<b>{1}</b>`").format(i.idx, i.leave_type), title="Duplicate Data")
+			else:
+				dup.setdefault(i.leave_type, 1)
