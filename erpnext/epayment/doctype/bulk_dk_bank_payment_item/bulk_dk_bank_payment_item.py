66d2241e618f33516d15a4069a4e8d95a0fa0b91
TW <tw@gmail.com>
2026-02-27 17:41:54 +0600

    bdbp

---
+++ erpnext/epayment/doctype/bulk_dk_bank_payment_item/bulk_dk_bank_payment_item.py
@@ -0,0 +1,30 @@
+# Copyright (c) 2026, Frappe Technologies Pvt. Ltd. and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class BulkDKBankPaymentItem(Document):
+	# begin: auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
+
+	from typing import TYPE_CHECKING
+
+	if TYPE_CHECKING:
+		from frappe.types import DF
+
+		amount: DF.Currency
+		bank_name: DF.Link | None
+		beneficiary_account_no: DF.Data | None
+		beneficiary_name: DF.Data | None
+		currency_code: DF.Link | None
+		description: DF.SmallText | None
+		dk_bank_payment: DF.Link | None
+		employee: DF.Link | None
+		parent: DF.Data
+		parentfield: DF.Data
+		parenttype: DF.Data
+		supplier: DF.Link | None
+	# end: auto-generated types
+	pass
