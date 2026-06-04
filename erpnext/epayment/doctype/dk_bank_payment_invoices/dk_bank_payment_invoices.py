44f31463ee14bbd4e40996b04c7ae92b955a51ab
pn <pn@gmail.com>
2026-06-04 09:24:12 +0600

    invoice

--- erpnext/epayment/doctype/dk_bank_payment_invoices/dk_bank_payment_invoices.py
+++ erpnext/epayment/doctype/dk_bank_payment_invoices/dk_bank_payment_invoices.py
@@ -1,11 +1,11 @@
-# Copyright (c) 2025, Frappe Technologies Pvt. Ltd. and contributors
+# Copyright (c) 2026, Frappe Technologies Pvt. Ltd. and contributors
 # For license information, please see license.txt
 
 # import frappe
 from frappe.model.document import Document
 
 
-class TransactionCode(Document):
+class DKBankPaymentInvoices(Document):
 	# begin: auto-generated types
 	# This code is auto-generated. Do not modify anything in this block.
 
@@ -14,8 +14,9 @@ class TransactionCode(Document):
 	if TYPE_CHECKING:
 		from frappe.types import DF
 
-		api: DF.Data | None
-		currency: DF.Link | None
-		trans_code: DF.Data | None
+		invoice: DF.Data | None
+		parent: DF.Data
+		parentfield: DF.Data
+		parenttype: DF.Data
 	# end: auto-generated types
 	pass
