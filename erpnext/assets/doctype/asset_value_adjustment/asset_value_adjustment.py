c3f0e72ba972e826de2938da98a98e07297d8e15
Tulasi-111 <tulasi.devi@thimphutechpark.bt>
2026-09-09 16:51:29 +0600

    asset value adjustment

--- erpnext/assets/doctype/asset_value_adjustment/asset_value_adjustment.py
+++ erpnext/assets/doctype/asset_value_adjustment/asset_value_adjustment.py
@@ -144,7 +144,7 @@ class AssetValueAdjustment(Document):
 
 		je = frappe.new_doc("Journal Entry")
 		je.voucher_type = "Journal Entry"
-		je.naming_series = "Journal Entry"
+		je.naming_series = "Journal Voucher"
 		je.posting_date = self.date
 		je.company = self.company
 		je.remark = f"Asset Adjustment Entry against {self.asset} worth {self.difference_amount}"
@@ -288,7 +288,7 @@ class AssetValueAdjustment(Document):
 			"user_remark": "Value (" + str(value) +" ) added to " + asset.name + " (" + asset.asset_name + ") ",
 			"posting_date": start_date,
 			"branch": asset.branch,
-			"naming_series":'Journal Entry'
+			"naming_series":'Journal Voucher'
 			})
 
 		#credit account update
